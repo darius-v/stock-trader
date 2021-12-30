730938a1b54f574ac00cc2c49e5a19524e504e27
Darius Valancauskas <darius.veracity@gmail.com>
2021-12-30 11:38:38 +0200

    Menu from component

---
+++ src/components/Menu.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Stock trader</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Portfolio <span class="sr-only">(current)</span></a></li>
+          <li><a href="#">Stocks</a></li>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">End day</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save and load <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Save</a></li>
+              <li><a href="#">Load</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Funds 10000</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+</template>
+
+<script>
+export default {
+  // props: {
+  //   value: {
+  //     type: String,
+  //     default: '',
+  //   }
+  // },
+  // computed: {
+  //   listeners () {
+  //     return {
+  //       // Pass all component listeners directly to input
+  //       ...this.$listeners,
+  //       // Override input listener to work with v-model
+  //       input: event => this.$emit('input', event.target.value)
+  //     }
+  //   }
+  // }
+}
+</script>
+
+<!--<style lang="scss" scoped>-->
+
+<!--</style>-->
--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  strict: true, // makes sure state is mutated only through mutations
+  state: {
+    funds: 0
+  },
+  mutations: {
+    increment (state, funds) {
+      state.funds = funds
+    }
+  },
+  getters: {
+    getFunds: state => {
+      return state.funds;
+    }
+  }
+});
+
+store.commit('increment', 9999);
+console.log(store.getters.getFunds);
+
 
 new Vue({
   el: '#app',
