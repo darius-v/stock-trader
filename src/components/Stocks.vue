90887c28c830e42f8a7a67d22103819cb774024c
Darius Valancauskas <darius.veracity@gmail.com>
2022-01-09 10:16:29 +0200

    Showing stocks for buy

--- src/components/Stocks.vue
+++ src/components/Stocks.vue
@@ -3,6 +3,16 @@
     <div class="panel-body">
       <h1>Stocks</h1>
 
+
+        <div class="panel panel-default col-md-6" v-for="stock in availableStocks">
+          <div class="panel-body">
+            {{ stock.name }}: {{ stock.price}}
+            <input type="number" v-model="stock.name">
+            <button @click="buy(stock.name)">Buy</button>
+          </div>
+        </div>
+
+<!--      <div class="col col-md-6">test</div>-->
     </div>
   </div>
 </template>
@@ -11,7 +21,21 @@
 export default {
   data() {
     return {
-      // funds: this.$store.getters.getFunds
+      amount: 0,
+      availableStocks: [
+        {name: 'Apple', price: 10000 },
+        {name: 'BMW', price: 10000 },
+        {name: 'FB', price: 300 },
+        {name: 'O', price: 60 }
+      ]
+    }
+  },
+
+  methods: {
+    buy(name) {
+      // todo get amount of stock - - maybe somehow pass as paremeter to buy funciton- but how?
+      let amount = this.name;
+      console.log(amount);
     }
   }
 }
